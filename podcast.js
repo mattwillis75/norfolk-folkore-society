3af163640184d9fd3ff6687010743ed825b3e593
mattwillis75 <thefolklorebox@gmail.com>
2026-07-25 14:38:14 +0100

    Add files via upload

---
+++ podcast.js
@@ -0,0 +1,23 @@
+const podcastArchive = [
+    {
+        title: "The Ghost Dogs of East Anglia",
+        publish_date: "2026-07-20",
+        description: "We explore the legend of Black Shuck, the demonic hound said to roam the Norfolk and Suffolk coastlines.",
+        image: "images/carousel-2.jpg",
+        spotify_link: "https://spotify.com" // Replace with the actual episode link
+    },
+    {
+        title: "Witches of King's Lynn",
+        publish_date: "2026-06-15",
+        description: "A deep dive into the 16th-century witch trials in King's Lynn, featuring local historian guest interviews.",
+        image: "images/carousel-3.jpg",
+        spotify_link: "https://spotify.com"
+    },
+    {
+        title: "The Lantern Men of the Fens",
+        publish_date: "2026-05-10",
+        description: "Beware the lights in the marshes. We discuss the eerie folklore of the will-o'-the-wisps luring travelers astray.",
+        image: "images/carousel-5.jpg",
+        spotify_link: "https://spotify.com"
+    }
+];
